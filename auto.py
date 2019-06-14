36963d52ce460460b7ed0064a8d0c577be308aa5
Thirafi Wian <thirafi.badai@gmail.com>
2019-06-14 12:54:37 +0700

    v1

--- auto.py
+++ auto.py
@@ -2,27 +2,42 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import json
 
+
+
 with open('text.json', 'r') as text:
     text_dict = json.load(text)
 
 for text in text_dict:
-    msg = text["text5"]
+	# Getting msg in json
+    msg1 = 	text["text8"]
 
-# Call executable app for chrome	
 driver = webdriver.Chrome("C:\\Chrome\\chromedriver.exe")
-# Enter the number here
 driver.get('https://web.whatsapp.com/send?phone=')
+											#  ^ Number goes here 
+
 
 #Scan the code before proceeding further
 input('Enter anything after scanning QR code to send messages')
 
-
-print("Sending message(s) ") 
-count = 5
-
-# Each box's class has different name, make sure that you check it first
+# Get the class from msg box
 msg_box = driver.find_element_by_class_name('_1PRhq')
+# chat_bubble = driver.find_element_by_class_name('-N6Gq')
 
-for i in range(count):
-    msg_box.send_keys(msg)
-    driver.find_element_by_class_name('_3M-N-').click()
+# print("reading chat",chat_bubble)
+def chooseMsg(choice):
+	switcher = {
+		1: msg1
+	}
+	return switcher.get(choice, "Invalid choice")
+
+while True:
+	inp = int(input('Input type of msg: '))
+	print('sendig this msg: ',chooseMsg(inp))
+	# for i in range(count):
+	print('sendig this msg: ',chooseMsg(inp))
+	msg_box.send_keys(chooseMsg(inp))
+	# driver.find_element_by_class_name('_3M-N-').click()
+	
+	q = input("Again ? y/n: ")
+	if q=='N' or q=='n':
+		break
